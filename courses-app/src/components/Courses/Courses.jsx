fabe40c492c625ea1bae4f694e7327b525e0ed67
Diego Jimenez2 <diego_jimenez2@epam.com>
2023-10-05 18:35:06 -0600

    adding Create Courses button

--- courses-app/src/components/Courses/Courses.jsx
+++ courses-app/src/components/Courses/Courses.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import CourseCard from './components/CourseCard';
 import SearchBar from './components/SearchBar/SearchBar';
-import Button from '../Header/components/Button/Button';
 
 function Courses() {
     return(
@@ -9,7 +8,6 @@ function Courses() {
             <div className='Courses'>
                 <div className='SearchBar'>
                     <SearchBar />
-                    <Button buttonText={'Add new course'}/>
                 </div>
                 <CourseCard />
             </div>
