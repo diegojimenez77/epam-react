60a75cc3dac67e3d6ee4a4920946a76963d376b0
Diego Jimenez2 <diego_jimenez2@epam.com>
2023-10-16 12:38:26 -0600

    commit with prettier and eslint configured

--- courses-app/src/components/Header/Header.jsx
+++ courses-app/src/components/Header/Header.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import Logo from './components/Logo/Logo'
-import Button from './components/Button/Button'
+import React from 'react';
+import Logo from './components/Logo/Logo';
+import Button from './components/Button/Button';
 
 const Header = () => (
-    <div className='header'>
-        <Logo />
-        <h3>Dave</h3>
-        <Button buttonText={'Logout'}/>
-    </div>
+	<div className='header'>
+		<Logo />
+		<h3>Dave</h3>
+		<Button buttonText={'Logout'} />
+	</div>
 );
 
-export default Header
+export default Header;
